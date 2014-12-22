a772c9d203b394596f3b7f408e731c4d329a74f1
Semyon Danilov <samvimes@yandex.ru>
2014-12-22 23:16:34 +0300

    asd

--- src/main/java/com/danilov/dbcourse/address/AddressRepository.java
+++ src/main/java/com/danilov/dbcourse/address/AddressRepository.java
@@ -8,7 +8,10 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Semyon on 04.11.2014.
@@ -79,6 +82,15 @@ public class AddressRepository {
                     .setParameter("street", street)
                     .setParameter("house", house)
                     .getSingleResult();
+        } catch (PersistenceException e) {
+            return null;
+        }
+    }
+
+    public List<Address> getUnsetDifferentStreets() {
+        try {
+            return entityManager.createQuery("SELECT a FROM Address a where region is null GROUP BY address HAVING count(*) > 1", Address.class)
+                    .getResultList();
         } catch (PersistenceException e) {
             return null;
         }
